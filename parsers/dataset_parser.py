8791385adbd057da5d9c11617d41393f8b968d22
Konstantinos Vytiniotis <konst.vyti@hotmail.com>
2024-02-04 23:22:59 +0200

    Dataset parser - update constructor

--- parsers/dataset_parser.py
+++ parsers/dataset_parser.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import librosa
+import matplotlib.pyplot as plt
+
 from abc import ABC, abstractmethod
 from os.path import exists
 
 
 class DatasetParser(ABC):
+    def __init__(self, _dataset_path):
+        if self.is_valid_path(_dataset_path):
+            self.dataset_path = _dataset_path
+            self.wavList = []
+        else:
+            raise ValueError('path.not_exists')
+
     def is_valid_path(self, path: str) -> bool:
         """_summary_
 
@@ -23,3 +34,15 @@ class DatasetParser(ABC):
         out of them
         """
         raise NotImplementedError()
+
+    def test(self):
+        fig = plt.figure(figsize=(15, 15))
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+        for i, label in enumerate(self.df['label'].unique()):
+            fn = self.df.loc[self.df['label'] == label]
+            fig.add_subplot(5, 2, i + 1)
+            plt.title(label)
+            data, sample_rate = librosa.load(fn)
+            librosa.display.waveplot(data, sr=sample_rate)
+        plt.savefig('class_examples.png')
