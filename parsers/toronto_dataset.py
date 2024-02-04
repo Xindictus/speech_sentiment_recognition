4175161476729971ddd5c1954b5016941d64c85e
Konstantinos Vytiniotis <konst.vyti@hotmail.com>
2024-02-04 23:27:27 +0200

    Handler for toronto parser

---
+++ parsers/toronto_dataset.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import os
+
+from toronto import TorontoParser
+
+
+# Define custom logger
+parser_logger = logging.getLogger('api_logger')
+
+# Define PY script folder
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def main():
+    dataset_path = f'{CURRENT_DIR}/datasets/toronto'
+    parser = TorontoParser(dataset_path)
+
+    df = parser \
+        .parse() \
+        .extract_features()
+
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
