d85a8a6d7f3142481cdc064534467f19d620f588
Konstantinos Vytiniotis <konst.vyti@hotmail.com>
2024-02-10 21:48:35 +0200

    Ravdess parser through class implementation

--- parsers/ravdess.py
+++ parsers/ravdess.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import librosa
 import numpy as np
+import os
+import pandas as pd
 
 from parsers.dataset_parser import DatasetParser
 
@@ -20,13 +23,59 @@ class RavdessParser(DatasetParser):
     COLS = {
         'file': str,
         'sample_rate': int,
+        'modality': int,
+        'vocal_channel': int,
+        'emotional intensity': int,
+        'statement': int,
+        'repetition': int,
+        'gender': str,
         'mfcc': np.array,
         'mel': np.array,
         'rms': np.array,
+        'spce': np.array,
         'stft': np.array,
         'zcr': np.array,
         'label': str,
     }
 
+    def get_gender(self, val):
+        return 'male' if val % 2 == 1 else 'female'
+
     def extract_features(self) -> None:
-        pass
+        data = []
+
+        for wav in self.wavList:
+            wav_parts = os.path.basename(wav).split('.')[0].split('-')
+            y, sr = librosa.load(wav)
+
+            """
+            WAV information retrieved from filename in order of `-` splits:
+                - modality (wav_parts[0])
+                - vocal channel (wav_parts[1])
+                - emotion (wav_parts[2])
+                - emotional intensity (wav_parts[3])
+                - statement (wav_parts[4])
+                - repetition (wav_parts[5])
+                - male/female actor/actress (wav_parts[6])
+            """
+            data.append([
+                wav,
+                sr,
+                int(wav_parts[0]),
+                int(wav_parts[1]),
+                int(wav_parts[3]),
+                int(wav_parts[4]),
+                int(wav_parts[5]),
+                self.get_gender(wav_parts[6]),
+                self.mfcc(y, sr),
+                self.mel(y, sr),
+                self.rms(y),
+                self.spce(y, sr),
+                self.stft(y),
+                self.zcr(y),
+                self.EMOTIONS(wav_parts[2])
+            ])
+
+            self.df = pd.DataFrame(data, columns=self.COLS)
+
+            return self
